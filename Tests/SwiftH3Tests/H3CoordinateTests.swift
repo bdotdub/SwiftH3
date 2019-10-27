b4d200fffa818dcf3c0f95ee5e7a80dac0351d84
Benny Wong <benny@bwong.net>
2019-10-27 14:32:04 -0400

    Convert H3Index ints into hex representations

--- Tests/SwiftH3Tests/H3CoordinateTests.swift
+++ Tests/SwiftH3Tests/H3CoordinateTests.swift
@@ -4,8 +4,8 @@ import XCTest
 final class H3CoordinateTests: XCTestCase {
     func testToIndex() {
         let coord = H3Coordinate(lat: 40.661, lon: -73.944)
-        XCTAssertEqual(coord.toIndex(resolution: 10), H3Index(622236751692857343))
-        XCTAssertEqual(coord.toIndex(resolution: 5), H3Index(599718753978023935))
+        XCTAssertEqual(coord.toIndex(resolution: 10), H3Index(0x8a2a10766d87fff))
+        XCTAssertEqual(coord.toIndex(resolution: 5), H3Index(0x852a1077fffffff))
     }
 
     static var allTests = [
--- Tests/SwiftH3Tests/H3IndexTests.swift
+++ Tests/SwiftH3Tests/H3IndexTests.swift
@@ -3,22 +3,22 @@ import XCTest
 
 final class H3IndexTests: XCTestCase {
     func testIsValid() {
-        XCTAssertTrue(H3Index(622236751692857343).isValid())
+        XCTAssertTrue(H3Index(0x8a2a10766d87fff).isValid())
         XCTAssertFalse(H3Index(0).isValid())
     }
 
     func testH3IndexToString() {
-        let index = H3Index(622236751692857343)
+        let index = H3Index(0x8a2a10766d87fff)
         XCTAssertEqual(index.toString(), "8a2a10766d87fff")
     }
 
     func testResolution() {
-        XCTAssertEqual(H3Index(622236751692857343).resolution, 10)
+        XCTAssertEqual(H3Index(0x8a2a10766d87fff).resolution, 10)
         XCTAssertEqual(H3Index(0).resolution, 0)
     }
 
     func testToCoord() {
-        let coord = H3Index(622236751692857343).toCoordinate()
+        let coord = H3Index(0x8a2a10766d87fff).toCoordinate()
         XCTAssertLessThan(abs(coord.lat - 40.66121200787385), 0.0001)
         XCTAssertLessThan(abs(coord.lon + 73.94380522623717), 0.0001)
     }
